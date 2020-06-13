361f17860096f2305799501494140399e79556d6
hao.lin <hao.lin@thoughtworks.com>
2020-06-13 11:29:54 +0800

    task 2

--- src/main/java/com/thoughtworks/tdd/Locker.java
+++ src/main/java/com/thoughtworks/tdd/Locker.java
@@ -1,8 +1,20 @@
 package com.thoughtworks.tdd;
 
-public class Locker {
+import java.util.HashMap;
+import java.util.Map;
 
-    public Ticket save(Bag bag) {
+public class Locker {
+    private final int capacity;
+    private final Map<Ticket, Bag> store;
+
+    public Locker(int capacity) {
+        this.capacity = capacity;
+        this.store = new HashMap<>(this.capacity);
+    }
+    public Ticket save(Bag bag) throws Exception {
+        if (store.size() >= capacity) {
+            throw new Exception("Locker is full");
+        }
         return new Ticket();
     }
 }
--- src/test/java/com/thoughtworks/tdd/LockerTest.java
+++ src/test/java/com/thoughtworks/tdd/LockerTest.java
@@ -4,20 +4,29 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 //DONE GIVEN LOCKER有空间，包 WHEN 存包 THEN 存包成功，得到票
-//TODO GIVEN LOCKER没有空间 WHEN 存包 THEN 存包失败
+//DONE GIVEN LOCKER没有空间 WHEN 存包 THEN 存包失败
 //TODO GIVEN 有效的票，WHEN 取包 THEN 取包成功
 //TODO GIVEN 取过包的有票 WHEN 取包 THEN 取包失败
 //TODO GIVEN 伪造票据 WHEN 取包 THEN 取包失败
 public class LockerTest {
 
     @Test
-    void should_get_ticket_given_not_full_locker_and_bag_when_save_bag() {
-        Locker locker = new Locker();
+    void should_get_ticket_given_not_full_locker_and_bag_when_save_bag() throws Exception {
+        Locker locker = new Locker(5);
         Bag bag = new Bag();
 
         Ticket ticket = locker.save(bag);
 
         assertNotNull(ticket);
+    }
 
+    @Test
+    void should_fail_given_full_locker_and_bag_when_save_bag() {
+        Locker locker = new Locker(0);
+        Bag bag = new Bag();
+
+        Exception exception = assertThrows(Exception.class, () -> locker.save(bag));
+
+        assertEquals("Locker is full", exception.getMessage());
     }
 }
