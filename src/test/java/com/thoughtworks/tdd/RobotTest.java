adad6e86bb8726a665c12b7ee6406f257f23cc58
qycai <1071226064@qq.com>
2020-06-14 21:32:05 +0800

    [qunyao]should throw TicketIsInvalidException when take given robot and two locker and invalid ticket

--- src/test/java/com/thoughtworks/tdd/RobotTest.java
+++ src/test/java/com/thoughtworks/tdd/RobotTest.java
@@ -12,8 +12,8 @@ import java.util.Arrays;
 //DONE GIVEN:机器人管理2个柜子，1号柜子满，2号柜子有剩余空间 WHEN: 存包 THEN: 包存到2号柜子，得到票
 //DONE GIVEN:机器人管理2个柜子，都无剩余 空间 WHEN: 存包 THEN: 存包失败
 //DONE GIVEN:机器人管理2个柜子，有效票 WHEN: 取包 THEN: 得到存的包
-//TODO GIVEN:机器人管理2个柜子，取过的票 WHEN: 取包 THEN: 取包失败
-//TODO GIVEN:机器人管理2个柜子，无效票 WHEN: 取包 THEN: 取包失败
+//DONE GIVEN:机器人管理2个柜子，取过的票 WHEN: 取包 THEN: 取包失败
+//DONE GIVEN:机器人管理2个柜子，无效票 WHEN: 取包 THEN: 取包失败
 public class RobotTest {
 
     @Test
@@ -81,5 +81,16 @@ public class RobotTest {
         robot.take(ticket);
 
         assertThrows(TicketIsInvalidException.class, () -> robot.take(ticket));
+    }
+
+    @Test
+    void should_throw_TicketIsInvalidException_when_take_given_robot_and_two_locker_and_invalid_ticket() throws LockerIsFullException, TicketIsInvalidException {
+        Locker locker1 = new Locker(2);
+        Locker locker2 = new Locker(4);
+        Robot robot = new Robot(Arrays.asList(locker1, locker2));
+        Bag givenBag = new Bag();
+        robot.save(givenBag);
+
+        assertThrows(TicketIsInvalidException.class, () -> robot.take(new Ticket()));
     }
 }
