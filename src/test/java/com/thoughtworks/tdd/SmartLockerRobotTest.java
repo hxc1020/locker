7633b3a84b1d4d663dccfd7d31ad1eb821cb7c9a
hao.lin <hao.lin@thoughtworks.com>
2020-06-18 22:53:02 +0800

    [linhao]should throw TicketIsInvalidException when take given smartLockerRobot manage locker1 and locker2 and primaryLockerRobot manage locker2 and locker3 and ticket which is saved by locker3 from primaryLockerRobot

--- src/test/java/com/thoughtworks/tdd/SmartLockerRobotTest.java
+++ src/test/java/com/thoughtworks/tdd/SmartLockerRobotTest.java
@@ -101,4 +101,19 @@ public class SmartLockerRobotTest {
 
         assertEquals(givenBag, bag);
     }
+
+    @Test
+    void should_throw_TicketIsInvalidException_when_take_given_smartLockerRobot_manage_locker1_and_locker2_and_primaryLockerRobot_manage_locker2_and_locker3_and_ticket_which_is_saved_by_locker3_from_primaryLockerRobot() throws LockerIsFullException {
+        Locker locker1 = new Locker(1);
+        Locker locker2 = new Locker(1);
+        Locker locker3 = new Locker(1);
+        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Arrays.asList(locker1, locker2));
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(Arrays.asList(locker2, locker3));
+        primaryLockerRobot.save(new Bag());
+        // locker2 is full , givenBag will saved to locker3
+        Bag givenBag = new Bag();
+        Ticket ticketFromLocker3 = primaryLockerRobot.save(givenBag);
+
+        assertThrows(TicketIsInvalidException.class, () -> smartLockerRobot.take(ticketFromLocker3));
+    }
 }
