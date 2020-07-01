154df163c87cfa03ec2c2848fe3e64d7ccd557c0
hao.lin <hao.lin@thoughtworks.com>
2020-07-01 20:01:10 +0800

    [linhao] refactor LockerRobotDirectorTest add createTestLocker method

--- src/test/java/com/thoughtworks/tdd/LockerRobotDirectorTest.java
+++ src/test/java/com/thoughtworks/tdd/LockerRobotDirectorTest.java
@@ -17,13 +17,10 @@ class LockerRobotDirectorTest {
             "given director manage 1 manager, manager manage locker1 and locker2, manager manage no robot," +
             "locker's available capacity and capacity is 0,3; 4,5 individually")
     void task1() throws LockerIsFullException {
-        LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(new Locker(3), new Locker(5)), Collections.emptyList());
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(
+                Arrays.asList(createTestLocker(0, 3), createTestLocker(4,5)),
+                Collections.emptyList());
         LockerRobotDirector lockerRobotDirector = new LockerRobotDirector(Collections.singletonList(lockerRobotManager));
-
-        lockerRobotManager.save(new Bag());
-        lockerRobotManager.save(new Bag());
-        lockerRobotManager.save(new Bag());
-        lockerRobotManager.save(new Bag());
 
         String report = lockerRobotDirector.getReport();
 
@@ -37,22 +34,11 @@ class LockerRobotDirectorTest {
             "manager manage no locker," +
             "locker's available capacity and capacity is 4,9; 2,5 individually")
     void task2() throws LockerIsFullException {
-        PrimaryLockerRobot primaryLockerRobot1 = new PrimaryLockerRobot(Collections.singletonList(new Locker(9)));
-        PrimaryLockerRobot primaryLockerRobot2 = new PrimaryLockerRobot(Collections.singletonList(new Locker(5)));
+        PrimaryLockerRobot primaryLockerRobot1 = new PrimaryLockerRobot(Collections.singletonList(createTestLocker(4, 9)));
+        PrimaryLockerRobot primaryLockerRobot2 = new PrimaryLockerRobot(Collections.singletonList(createTestLocker(2, 5)));
         LockerRobotManager lockerRobotManager = new LockerRobotManager(Collections.emptyList(),
                 Arrays.asList(primaryLockerRobot1, primaryLockerRobot2));
         LockerRobotDirector lockerRobotDirector = new LockerRobotDirector(Collections.singletonList(lockerRobotManager));
-
-        primaryLockerRobot1.save(new Bag());
-        primaryLockerRobot1.save(new Bag());
-        primaryLockerRobot1.save(new Bag());
-        primaryLockerRobot1.save(new Bag());
-        primaryLockerRobot1.save(new Bag());
-
-        primaryLockerRobot2.save(new Bag());
-        primaryLockerRobot2.save(new Bag());
-        primaryLockerRobot2.save(new Bag());
-
 
         String report = lockerRobotDirector.getReport();
 
@@ -66,14 +52,10 @@ class LockerRobotDirectorTest {
             "manager manage no locker," +
             "locker's available capacity and capacity is 0,2; 0,1 individually")
     void task3() throws LockerIsFullException {
-        PrimaryLockerRobot primaryLockerRobot1 = new PrimaryLockerRobot(Collections.singletonList(new Locker(1)));
-        LockerRobotManager lockerRobotManager = new LockerRobotManager(Collections.singletonList(new Locker(2)),
+        PrimaryLockerRobot primaryLockerRobot1 = new PrimaryLockerRobot(Collections.singletonList(createTestLocker(0, 1)));
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(Collections.singletonList(createTestLocker(0, 2)),
                 Collections.singletonList(primaryLockerRobot1));
         LockerRobotDirector lockerRobotDirector = new LockerRobotDirector(Collections.singletonList(lockerRobotManager));
-
-        lockerRobotManager.save(new Bag());
-        lockerRobotManager.save(new Bag());
-        lockerRobotManager.save(new Bag());
 
         String report = lockerRobotDirector.getReport();
 
@@ -87,18 +69,14 @@ class LockerRobotDirectorTest {
             "manager2 manage locker3," +
             "locker's available capacity and capacity is 2,3; 1,2;0,2 individually")
     void task4() throws LockerIsFullException {
-        PrimaryLockerRobot primaryLockerRobot1 = new PrimaryLockerRobot(Collections.singletonList(new Locker(2)));
-        Locker locker1 = new Locker(3);
+        PrimaryLockerRobot primaryLockerRobot1 = new PrimaryLockerRobot(Collections.singletonList(createTestLocker(1, 2)));
+        Locker locker1 = createTestLocker(2, 3);
         LockerRobotManager lockerRobotManager1 = new LockerRobotManager(Collections.singletonList(locker1),
                 Collections.singletonList(primaryLockerRobot1));
-        LockerRobotManager lockerRobotManager2 = new LockerRobotManager(Collections.singletonList(new Locker(2)), Collections.emptyList());
+        LockerRobotManager lockerRobotManager2 = new LockerRobotManager(
+                Collections.singletonList(createTestLocker(0, 2)),
+                Collections.emptyList());
         LockerRobotDirector lockerRobotDirector = new LockerRobotDirector(Arrays.asList(lockerRobotManager1, lockerRobotManager2));
-
-        locker1.save(new Bag());
-        primaryLockerRobot1.save(new Bag());
-        lockerRobotManager2.save(new Bag());
-        lockerRobotManager2.save(new Bag());
-
 
         String report = lockerRobotDirector.getReport();
 
@@ -112,19 +90,23 @@ class LockerRobotDirectorTest {
             "locker's available capacity and capacity is 1,3; 5,6 individually，" +
             "and there is a locker3 which is not managed by manager ")
     void task5() throws LockerIsFullException {
-        Locker locker1 = new Locker(3);
-        Locker locker2 = new Locker(6);
+        Locker locker1 = createTestLocker(1, 3);
+        Locker locker2 = createTestLocker(5, 6);
         LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(locker1, locker2),
                 Collections.emptyList());
         LockerRobotDirector lockerRobotDirector = new LockerRobotDirector(Collections.singletonList(lockerRobotManager));
         Locker locker3 = new Locker(4);
 
-        locker1.save(new Bag());
-        locker1.save(new Bag());
-        locker2.save(new Bag());
-
         String report = lockerRobotDirector.getReport();
 
         assertEquals("M  6  9\n    L  1  3\n    L  5  6", report);
+    }
+
+    private Locker createTestLocker(int availableCapacity, int capacity) throws LockerIsFullException {
+        Locker locker = new Locker(capacity);
+        for (int i = availableCapacity; i < capacity; i++) {
+            locker.save(new Bag());
+        }
+        return locker;
     }
 }
